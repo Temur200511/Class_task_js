21cd86e70b7e2e8fa83591ca5db0ac43380327b4
Temur200511 <tziyodullayev61@gmail.com>
2025-01-12 10:38:36 +0500

    compl

--- app.js
+++ app.js
@@ -1,36 +1,3 @@
-// class Animal{
-//     constructor(name, type, sound){
-//         this.name=name;
-//         this.type=type;
-//         this.sound=sound;
-//     }
-
-//      set makesound(cat){
-//        console.log(`${this.name} is making sound`);
-//     }
-  
-
-// }
-
-// const Animal1 = new Animal("wolf", "Catus", "wuuu")
-
-// class Cat extends Animal{
-//     constructor(name, type, sound, color){
-//        this.name=name;
-//         super(type);
-//         this.sound=sound;
-//         this.color=color;
-//     }
-
-//  get fullInfo(){
-//     console.log(`${this.name} is running, its type is ${this.type}, its sound is ${this.sound} color is ${this.color}`);
-//  }
-// }
-
-// const Cat1= new Cat("Muska", "Catus", "Miaw", "Black")
-
-
-
 
 
 
@@ -113,3 +80,67 @@
   
 //   console.log(`${student.name}ning o'rtacha bahosi:`, student.getAverageGrade()); 
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Animal{
+//     constructor(name, type, sound){
+//         this.name=name;
+//         this.type=type;
+//         this.sound=sound;
+//     }
+
+//      set makesound(cat){
+//        console.log(`${this.name} is making sound`);
+//     }
+  
+
+// }
+
+// const Animal1 = new Animal("wolf", "Catus", "wuuu")
+
+// class Cat extends Animal{
+//     constructor(name, type, sound, color){
+//        this.name=name;
+//         super(type);
+//         this.sound=sound;
+//         this.color=color;
+//     }
+
+//  get fullInfo(){
+//     console.log(`${this.name} is running, its type is ${this.type}, its sound is ${this.sound} color is ${this.color}`);
+//  }
+// }
+
+// const Cat1= new Cat("Muska", "Catus", "Miaw", "Black")
+
+
+
