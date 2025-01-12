a81afdb761e8b68cd48b5057daf79e3e6f563a5a
Temur200511 <tziyodullayev61@gmail.com>
2025-01-12 10:36:32 +0500

    full

--- app.js
+++ app.js
@@ -41,46 +41,75 @@
 
 
 
-
-class Animal {
-    constructor(name, type, sound) {
-      this.name = name; 
-      this.type = type; 
-      this.sound = sound; 
-    }
+// 1
+// class Animal {
+//     constructor(name, type, sound) {
+//       this.name = name; 
+//       this.type = type; 
+//       this.sound = sound; 
+//     }
   
     
-    makeSound() {
-      console.log(`${this.name} ovoz chiqardi: ${this.sound}`);
-    }
-  }
+//     makeSound() {
+//       console.log(`${this.name} ovoz chiqardi: ${this.sound}`);
+//     }
+//   }
   
-  class Cat extends Animal {
-    constructor(name) {
-      super(name, "mushuk", "meow"); 
-    }
+//   class Cat extends Animal {
+//     constructor(name) {
+//       super(name, "mushuk", "meow"); 
+//     }
   
-    tirnash() {
-      console.log(`${this.name} tirnoq ishlatyapti!`);
-    }
-  }
+//     tirnash() {
+//       console.log(`${this.name} tirnoq ishlatyapti!`);
+//     }
+//   }
   
-  class Dog extends Animal {
-    constructor(name) {
-      super(name, "it", "woof"); 
-    }
+//   class Dog extends Animal {
+//     constructor(name) {
+//       super(name, "it", "woof"); 
+//     }
   
-    tayoqolibkelish() {
-      console.log(`${this.name} tayoq olib kelyapti!`);
-    }
-  }
+//     tayoqolibkelish() {
+//       console.log(`${this.name} tayoq olib kelyapti!`);
+//     }
+//   }
   
-  // Misol
-  const myCat = new Cat("Mushukcha");
-  myCat.makeSound(); 
-  myCat.tirnash(); 
+//   // Misol
+//   const myCat = new Cat("Mushukcha");
+//   myCat.makeSound(); 
+//   myCat.tirnash(); 
   
-  const myDog = new Dog("Bobik");
-  myDog.makeSound(); 
-  myDog.tayoqolibkelish(); 
+//   const myDog = new Dog("Bobik");
+//   myDog.makeSound(); 
+//   myDog.tayoqolibkelish(); 
+  
+
+
+
+// 2
+// class Student {
+//     constructor(name, age) {
+//       this.name = name; 
+//       this.age = age; 
+//       this.grades = []; 
+//     }
+  
+//     addGrade(grade) {
+//       this.grades.push(grade); 
+//     }
+  
+//     getAverageGrade() {
+//       const total = this.grades.reduce((sum, grade) => sum + grade, 0);
+//       return this.grades.length > 0 ? total / this.grades.length : 0; 
+//     }
+//   }
+  
+
+//   const student = new Student("Ali", 21);
+//   student.addGrade(90); 
+//   student.addGrade(85);
+//   student.addGrade(88); 
+  
+//   console.log(`${student.name}ning o'rtacha bahosi:`, student.getAverageGrade()); 
   
